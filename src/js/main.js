b1f8f0e1c2d85469a8646477a9674b02053d9b6e
SergeyPie <davidwayne278@gmail.com>
2019-05-17 16:38:20 +0300

    added twitter,footer,pricing sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,34 @@
 
 var menu = document.querySelector('.nav-gamburger');
 
-menu.addEventListener("click", function () {
-  var header = document.querySelector('.header');
+var sectionPricing = document.querySelector('.section-pricing');
+var sectionPricingTitles = document.querySelector('.section-pricing__titles');
 
-  header.classList.toggle("header-mobile");
-  menu.classList.toggle("nav-gamburger_active");
+
+// addFixedTitle(sectionPricing, sectionPricingTitles) 
+// window.addEventListener("scroll", function () {
+//     addFixedTitle(sectionPricing, sectionPricingTitles) 
+// });
+
+
+
+
+
+
+
+// function addFixedTitle(wrapper, elementFixed) {
+//     if (wrapper.getBoundingClientRect().top < 0 && wrapper.getBoundingClientRect().bottom > 0) {
+//         elementFixed.classList.add('fixed')
+//     }
+//     else {
+//         elementFixed.classList.remove('fixed')
+//     }
+// }
+
+
+
+menu.addEventListener("click", function () {
+    var header = document.querySelector('.header');
+    header.classList.toggle("header-mobile");
+    menu.classList.toggle("nav-gamburger_active");
 });
